c30bae3e39e00f2bc9dee3e501752814a8a38a59
Michael <michaelkmb@hotmail.com>
2023-09-28 00:12:41 -0300

    + filter for Json; + html for filter;

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const searchBtn = document.getElementById("btnSearch");
 const content = document.getElementById("content");
 const img = document.getElementById("img");
 
+const filters = document.getElementsByClassName(".cbox__inp");
+
 const baseUrlApi = "https://rickandmortyapi.com/api";
 const endpointApi = "/character/";
 
@@ -12,10 +14,32 @@ const fetchApi = (value) => {
     return result;
 }
 
+const keys = ['name', 'status', 'species', 'gender',
+                'origin', 'image', 'episode'];
+
+const newKeys = {
+    name: 'Nome',
+    status: 'Estado',
+    species: 'Espécie',
+    gender: 'Gênero',
+    origin: 'Planeta de origem',
+    episode: 'Episódios',
+};
+
+const buildResult = (result) => {
+    const newObject = {};
+    keys.map( (key) => document.getElementById(key))
+        .map((elem) => elem.checked
+                && (newObject[elem.name] = result[elem.name]));
+    console.log(keys);
+    console.log(newObject);
+    return newObject;
+}
+
 searchBtn.addEventListener("click", async (e) => {
     e.preventDefault();
     const result = await fetchApi(characterId.value);
-    content.textContent = `${JSON.stringify(result, undefined, 2)}`;
+    content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`;
 
     img.src = `${result.image}`;
-})
+});
